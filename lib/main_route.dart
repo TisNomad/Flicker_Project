8b9d8658f76c8477dcc3cf7291961fcc45dfa1e7
TisNomad <lpmoguzhan2@gmail.com>
2022-08-25 14:53:40 +0300

    cleared code weight in main.dart by separeting to different files

---
+++ lib/main_route.dart
@@ -0,0 +1,172 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wakelock/wakelock.dart';
+
+import 'global_data.dart';
+import 'my_painter.dart';
+import 'settings_route.dart';
+
+class MainRoute extends StatefulWidget {
+  const MainRoute({Key? key}) : super(key: key);
+
+  @override
+  State<MainRoute> createState() => _MainRouteState();
+}
+
+class _MainRouteState extends State<MainRoute> {
+  void handleDrowDownMenu(String choice) {
+    if (choice == "Settings") {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => Settings()));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Wakelock.enable();
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: handleDrowDownMenu,
+            itemBuilder: (BuildContext context) {
+              return {"Settings"}.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            },
+          )
+        ],
+        centerTitle: true,
+        title: const Text("Flicker Project"),
+      ),
+      body: MyCanvas(),
+    );
+  }
+}
+
+class MyCanvas extends StatefulWidget {
+  MyCanvas({Key? key}) : super(key: key);
+
+  @override
+  State<MyCanvas> createState() => _MyCanvasState();
+}
+
+class _MyCanvasState extends State<MyCanvas> {
+  bool isAnimating = false;
+  var globalFlickerList = [];
+  Timer? timer;
+  @override
+  @override
+  Widget build(BuildContext context) {
+    //print("MyCanvas build method called.");
+    final globalFlickerList = context.watch<GlobalData>().flickerList;
+    Color backGroundColor =
+        Provider.of<GlobalData>(context, listen: false).backGroundColor;
+    Color increaseButtonColor = context.watch<GlobalData>().increaseColor;
+    Color decreaseButtonColor = context.watch<GlobalData>().decreaseColor;
+    Key increaseButtonKey = const Key("inc");
+    Key decreaseButtonKey = const Key("dec");
+
+    return Column(
+      children: [
+        Expanded(
+          flex: 10,
+          child: GestureDetector(
+            onTapDown: (TapDownDetails details) {
+              toggleHzIncrease();
+            },
+            child: Container(
+              color: backGroundColor,
+              child: CustomPaint(
+                painter: MyPainter.acceptList(globalFlickerList),
+                child: Container(),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            color: backGroundColor,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                // INCREASE BUTTON ----------
+                Expanded(
+                  child: OutlinedButton.icon(
+                    icon: const Icon(
+                      Icons.add,
+                      color: Colors.blue,
+                    ),
+                    key: increaseButtonKey,
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: increaseButtonColor,
+                    ),
+                    label: Text("Increase"),
+                    onPressed: () {
+                      setState(() {
+                        toggleHzIncrease();
+                      });
+                    },
+                  ),
+                ),
+
+                // DECREASE BUTTON -------------
+                Expanded(
+                  child: OutlinedButton.icon(
+                    icon: const Icon(
+                      Icons.remove,
+                      color: Colors.blue,
+                    ),
+                    key: decreaseButtonKey,
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: decreaseButtonColor,
+                    ),
+                    label: Text("Decrease"),
+                    onPressed: () {
+                      setState(() {
+                        toggleHzDecrease();
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  void toggleHzIncrease() {
+    context.read<GlobalData>().toggleHzIncrease();
+  }
+
+  void toggleHzDecrease() {
+    context.read<GlobalData>().toggleHzDecrease();
+  }
+
+  //Toggles animation of the flicker objects
+  void toggleAnim() {
+    print("toggleAnim() method called.");
+    context.read<GlobalData>().toggleFlicker(
+        secondaryColor: context.read<GlobalData>().backGroundColor);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    print("initState() called from MyCanvas");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    Wakelock.disable();
+  }
+}
---
+++ lib/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:hello_world/main.dart';
+
+import 'main_route.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainRoute(),
+    );
+  }
+}

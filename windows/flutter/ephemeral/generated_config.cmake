626ae319599bb18ef1449b34bd86a2b6e43ae47e
TisNomad <lpmoguzhan2@gmail.com>
2022-08-29 15:45:21 +0300

    macos files

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ouyanik\\StudioProjects\\hello_world" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "PROJECT_DIR=C:\\Users\\ouyanik\\StudioProjects\\hello_world"
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ouyanik\\StudioProjects\\hello_world\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ouyanik\\StudioProjects\\hello_world"
+  "FLUTTER_TARGET=C:\\Users\\ouyanik\\StudioProjects\\hello_world\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ouyanik\\StudioProjects\\hello_world\\.dart_tool\\package_config.json"
+)
